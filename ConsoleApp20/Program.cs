f243fc83cb1859398e174d834e386dab123447b0
totoqqwe <anton.stepanchenko14@gmail.com>
2023-12-12 12:33:22 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int, double> average = delegate (int a, int b, int c)
+            {
+                return (a + b + c) / 3.0;
+            };
+
+            int num1 = 10, num2 = 20, num3 = 30;
+            double result = average(num1, num2, num3);
+
+            Console.WriteLine($"Середнє арифметичне {num1}, {num2}, {num3} = {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
